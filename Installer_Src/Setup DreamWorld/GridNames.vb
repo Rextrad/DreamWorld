9d9ecbf6b639de258a6b68d856f854be32be3484
Fred Beckhusen <fred@outworldz.com>
2023-01-15 18:37:45 -0600

    Simplfy and correct external Ports

--- Installer_Src/Setup DreamWorld/GridNames.vb
+++ Installer_Src/Setup DreamWorld/GridNames.vb
@@ -11,22 +11,14 @@ Module GridNames
 
         If Settings.ServerType = RobustServerName Then
             TextPrint("--> " & My.Resources.Server_Type_is & " Robust")
-            Settings.ExternalHostName = Settings.PublicIP
         ElseIf Settings.ServerType = OsgridServer Then
             Settings.DnsName = "hg.osgrid.org"
             Settings.BaseHostName = "hg.osgrid.org"
-            Settings.ExternalHostName = WANIP()
             TextPrint("--> " & My.Resources.Server_Type_is & " OSGrid")
         ElseIf Settings.ServerType = RegionServerName Then
-            If Settings.OverrideName.Length > 0 Then
-                Settings.ExternalHostName = Settings.OverrideName
-            Else
-                Settings.ExternalHostName = Settings.PublicIP
-            End If
             TextPrint("--> " & My.Resources.Server_Type_is & " Region")
         ElseIf Settings.ServerType = MetroServer Then
             Settings.DnsName = "hg.metro.land"
-            Settings.ExternalHostName = Settings.PublicIP
             Settings.BaseHostName = "hg.metro.land"
             TextPrint("--> " & My.Resources.Server_Type_is & " Metro")
         End If
@@ -36,7 +28,7 @@ Module GridNames
             TextPrint("--> Region IP=" & Settings.ExternalHostName)
         ElseIf Settings.ServerType = RobustName() Then
             Settings.ExternalHostName = Settings.PublicIP
-            TextPrint("--> Region IP=" & Settings.ExternalHostName)
+            TextPrint("--> Region IP=" & Settings.PublicIP)
         End If
 
     End Sub
